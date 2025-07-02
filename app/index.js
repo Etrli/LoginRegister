81cd88a815558b4f83b8f67cc32590c2b9df9438
Eric <patinetakiller564@gmail.com>
2025-07-02 15:51:09 -0300

    Se creo la ruta register

--- app/index.js
+++ app/index.js
@@ -3,14 +3,29 @@ import path from "path"
 import { fileURLToPath } from "url"
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
+//server
 const app = express()
 const port = 4000;
-
-
 
 
 app.listen(port, () => {
   console.log(`En el http://localhost:${port}`)
 })
 
+
+
+//config
+app.use(express.static(__dirname+ "/public"))
+
+
+
+//rutas
 app.get("/",(req,res)=>res.sendFile(__dirname + "/pages/login.html"));
+app.get("/register",(req,res)=>res.sendFile(__dirname + "/pages/register.html"));
+
+
+
+
+
+
+
